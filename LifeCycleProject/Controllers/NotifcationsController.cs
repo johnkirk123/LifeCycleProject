8b19cfba344b4f01e4e0bcc3f53014538cceb94e
Jeffrey McCuch <tuk02734@temple.edu>
2024-04-18 18:35:21 -0400

    Notifications Views/Controllers

---
+++ LifeCycleProject/Controllers/NotifcationsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LifeCycleProject.Controllers
+{
+    public class NotifcationsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
