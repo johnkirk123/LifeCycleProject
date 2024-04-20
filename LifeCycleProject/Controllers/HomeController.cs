cba23ca72f5b9b9cad35b2d0aec22fe21ae240ce
vanessa523 <tun32988@temple.edu>
2024-04-20 18:13:06 -0400

    Admin and Student Project/Landing Views done. He's gonna GRILL me because the UI here is set up to look nice, not to be super functional for displaying/storing documents, but I'm still confused on all the blobtype stuff and this is due in 2 days lol

--- LifeCycleProject/Controllers/HomeController.cs
+++ LifeCycleProject/Controllers/HomeController.cs
@@ -18,6 +18,7 @@ namespace LifeCycleProject.Controllers
             return View();
         }
 
+
         public IActionResult Privacy()
         {
             return View();
---
+++ LifeCycleProject/Controllers/StudentProjectLandingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LifeCycleProject.Controllers
+{
+    public class StudentProjectLandingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
