b4cd41f4a56c286e578a11ee5cab5a5989d1db97
John A Kirkpatrick <tun34826@temple.edu>
2024-04-22 18:28:42 -0400

    Added user class

---
+++ LifeCycleProject/Models/User.cs
@@ -0,0 +1,74 @@
+﻿namespace LifeCycleProject.Models
+{
+    public class User
+    {
+     
+        private int userID;
+        private int roleID;
+        private string userPassword;
+        private string firstName;
+        private string lastName;
+        private string email;
+        private string phone;
+        private string profileImage;
+
+        public int UserID 
+        { 
+            get { return userID; } 
+            set { userID = value; } 
+        }
+        public int RoleID 
+        { 
+            get { return roleID; } 
+            set {  roleID = value; } 
+        }
+        public string UserPassword 
+        { 
+            get {  return userPassword; } 
+            set { userPassword = value; } 
+        }
+
+        public string FirstName 
+        { 
+            get { return firstName; } 
+            set { firstName = value; } 
+        }
+        public string LastName 
+        { 
+            get { return lastName; } 
+            set { lastName = value; } 
+        }
+        public string Email 
+        { 
+            get { return email; } 
+            set { email = value; } 
+        }
+        public string Phone 
+        { 
+            get { return phone; } 
+            set { phone = value; } 
+        }
+        public string ProfileImage 
+        { get {  return profileImage; } 
+          set {  profileImage = value; } 
+        }
+
+        public User()
+        {
+
+        }
+
+        public User(int userID, int roleID, string userPassword, string firstName, string lastName, string email, string phone, string profileImage)
+        {
+            UserID = userID;
+            RoleID = roleID;
+            UserPassword = userPassword;
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            Phone = phone;
+            ProfileImage = profileImage;
+            
+        }
+    }
+}
