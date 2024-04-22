bba603d0c5dfed0dc804f473099c7551755aa929
Amy <amysnodgrass927@gmail.com>
2024-04-22 16:50:49 -0400

    student documents done

---
+++ LifeCycleProject/Controllers/PendingStudentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LifeCycleProject.Controllers
+{
+    public class PendingStudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
